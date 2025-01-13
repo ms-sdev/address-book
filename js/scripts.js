2cd76bc306949ac2687336bd4cab135c8d1115aa
ms-sdev <stella.cherotich@moringaschool.com>
2025-01-13 10:54:03 +0300

    Updating scripts js with setting IDs content

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-function Contact(firstName, lastName, phoneNumber) {
+// Business Logic for AddressBook ---------
+function AddressBook() {
+    this.contacts = {};
+    this.currentId = 0;
+  }
+  
+  AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts[contact.id] = contact;
+  };
+  
+  AddressBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+  };
+  
+  // Business Logic for Contacts ---------
+  function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
